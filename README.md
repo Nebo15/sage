b9663e46f30d13de76e602afc0897be7779eeea6
Andrew Dryga <andrew@dryga.com>
2017-10-31 15:24:50 +0200

    Spec some suggestions and RFC's

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 All guarantees that transaction steps (independent atomic actions) are completed or all failed steps are compensated.
 
+system guarantees that either all the transactions m a saga are successfully completed or compensatmg transactions are run to amend a partial execution
+
 This is a library for Elixir that implements the [Saga][saga-paper] pattern for
 error recovery/cleanup in distributed transactions. The saga pattern describes
 two call flows, a forward flow that represents progress, and an opposite
@@ -24,132 +26,7 @@ rollback flow which represents recovery and cleanup activities.
 
 Sage allows you to use adapter to simplify integration with database and HTTP clients.
 
-## Terminology
-
-# The database management
-```
-# system guarantees that either all the transactions m a saga are successfully completed or compensatmg transactions are run to amend a partial execution
-transaction(..., compensate_transaction :: function)
-
-```
-
-### Direct usage
-
-```elixir
-Sage.new()
-|> Sage.do(:create_user,
-  fun _state, _opts -> HTTPAdapter.post("http://example.com/abc") end,
-  fun %{create_user: created_user}, _opts ->
-    HTTPAdapter.delete("http://example.com/abc/#{created_user[:user_id]")
-  end
-)
-|> Sage.do(:add_to_subscribers, after: [:create_user],
-  fun %{create_user: created_user}, %{list_id: list_id} ->
-    HTTPAdapter.post("http://example.com/lists/#{list_id}/subscribers", created_user)
-  end,
-  fun %{add_to_subscribers: subscriber}, %{list_id: list_id} ->
-    HTTPAdapter.delete("http://example.com/lists/#{list_id}/subscribers/#{subscriber[:subscribe_id]")
-  end
-)
-|> Sage.do(:send_welcome_email, after: [:create_user],
-  fun %{create_user: created_user}, _opts ->
-    HTTPAdapter.post("http://example.com/send_email", %{to: created_user.email, body: "Hello there!"})
-  end
-)
-|> Sage.execute(%{list_id: 123})
-```
-
-or
-
-```elixir
-defmodule MySage do
-  def execute(opts) do
-    Sage.new()
-    |> Sage.do(:create_user, retries: 3, timeout: 5000)
-    |> Sage.do(:add_to_subscribers)
-    |> Sage.do(:send_welcome_email)
-    |> Sage.execute(opts)
-  end
-
-  defp create_user(:forward, state, opts) do
-    opts[:http_adapter].post("http://example.com/abc")
-  end
-  defp create_user(:rollback, state, opts)
-
-  defp add_to_subscribers(:forward, state, opts)
-  defp add_to_subscribers(:rollback, state, opts)
-
-  defp send_welcome_email(:forward, state, opts)
-  defp send_welcome_email(:rollback, state, opts)
-end
-
-MySage.execute(http_adapter: HTTPoison)
-```
-
-# Interface
-
-```
-@type pipeline :: %{}
-@type step :: %{}
-@type retry_opts :: [retry_limit: 3, retry_backoff: :exponential, retry_base_timeout: 1000]
-@type sage_apply :: fn prev_results, state
-@type sage_rollback :: fn apply_result, state
-
-@type finally :: fn {:ok, result, other_side_effects}, state -> | fn {:error, failed_step_name :: atom}, state ->
-
-@spec new(shared_state :: any) :: pipeline
-@spec retry(pipeline :: pipeline, step :: Sage.step, opts :: retry_opts) :: pipeline
-@spec run(pipeline :: pipeline, name :: atom, apply :: sage_apply, rollback :: sage_rollback) :: pipeline
-@spec run_cached(pipeline :: pipeline, name :: atom, apply :: sage_apply, rollback :: sage_rollback) :: pipeline
-@spec parallel(pipeline :: pipeline, parallel1 :: pipeline, parallel2 :: pipeline, ..) :: pipeline
-
-@spec finally(result :: pipeline, callback :: function) :: pipeline # Should execute even if there is a bug in the code
-
-@spec execute(pipeline :: pipeline) :: {:ok, result, [other_side_effects]} | {:error, reason}
-
-@spec isolate(pipeline :: pipeline, before :: function, rollback :: function) # isolate(sage, Repo.transaction/1, Repo.rollback/1)
-
-@spec run_multi(pipeline :: pipeline, Ecto.Multi.t, error_handler :: function) :: pipeline
-@spec from_multi(Ecto.Multi.t, error_handler :: function)
-@spec with_idempotency(key :: string, adapter :: module)
-
-@spec append
-@spec prepend
-@spec merge(pipeline, pipeline | {m, f, a})
-
-# Ideas
-@spec abort
-@spec run_async()
-# run_async() |> run_async() |> run() - run() would await for all async operations to complete, instead of parallel()
-
-
-@spec checkpoint(retry_opts) # Retry everything between checkpoints
-@spec forward_recovery(retry_opts) # Called from a compensating transaction block, it tells how many times we want to retry upstream transactions before failure ## can be in format of returning {:retry, blah blah} from it, which makes `checkpoint/2` very simple. No tx code, but retry logic in rollback code.
-
-@type rollback_return :: {:error, reason} # I am compensated my transactions, continue backwards-recovery
-                       | {:ok, :retry} # I am compensated by transaction, let's retry
-                       | {:abort, reason} # I am compensated transaction and want to force backwards recovery on all steps
-```
-
-*Error is returned from the latest compensating transaction. We don't care about compensation in between.*
-
-```
-defimpl Sagas, for: Ecto.Multi do
-  @impl true
-  def run(multi) do
-    from_multi(multi)
-  end
-
-  defp from_multi(%Ecto.Multi{}), do: ...
-end
-
-```
-
 # RFC's
-
-### Checkpoints
-
-Do not put names in `Saga.do/2`, instead use `Saga.checkpoint/3`.
 
 ### Side effects
 
@@ -164,59 +41,22 @@ Sage.new()
 |> Sage.do(..) # Error, probably nothing to rollback and retry of this sage would continue from this step (state is fetched from DB)
 |> Sage.do(..)
 
-### Parallel execution
-
-```
-s1 = Sage.new() |> Sage.do(...) |> Sage.do(...)
-s2 = Sage.new() |> Sage.do(...) |> Sage.do(...)
-
-Sage.new() |> Sage.do |> Sage.parallel(s1, s2) |> Sage.do(...) |> Sage.run()
-```
-
-### Before and Finally
-
-Sometimes we want to run function on beginning and end of sage irrespectively to the individual execution stages (about the same as Ecto.Multu returns you `{:ok, result}` or error stage which is failed).
-
-This is especially useful to init a DB transaction and rollback it after all execution stages:
-
-```
-Wrap execution in Repo.transaction and rollback it if pipeline fails
-Sage.do(apply, rollback, fn _ -> Repo.transaction() end, fn {:error, reason} -> Repo.rollback() end)
-```
-
-Or you want to ack or nack to a message queue:
-
-```
-Wrap execution in Repo.transaction and rollback it if pipeline fails
-Sage.do(apply, rollback, fn _ -> AMQP.ack() end, fn {:error, reason} -> AMQP.nack() end)
-```
-
-### Retries
-
-Ability to set a retry policy for an individual stage, eg:
-
-`Sage.do(..., retry_limit: 3, retry_backoff: :exponential, retry_base_timeout: 1000)`
-
 ### HTTP lib on top of Sage
 
-See https://gist.github.com/michalmuskala/5cee518b918aa5a441e757efca965d22
+See https://gist.github.com/michalmuskala/5cee518b918aa5a441e757efca965d22.
 
-#### Request Cache
+HTTP client can extend Saga's interface and we will be able to:
+- suggest the way to compensate request;
+- require compensation for non-idempotent operations;
+- reduce amount of code that needs to be written for complex API interactions.
 
-`do_cached/2`
+#### Type checking
 
-### Macro syntax
+We can leverage dializer?
 
-1.
+#### Tests
 
-```
-Sage.new()
-|-> apply()
-|-< rollback()
-|-> apply()
-|-< rollback()
-|> Sage.execute()
-```
+Integration with property testing would make possible to test your transaction functions and almost automatically make sure Saga is correct for most common scenarios.
 
 ### Ecto.Multi replacement & Database integrations & Runnin saga in Repo.transaction(...)
 
--- lib/sage.ex
+++ lib/sage.ex
@@ -2,7 +2,67 @@ defmodule Sage do
   @moduledoc """
   Documentation for Sage.
 
-  - `before_and_after_state` is shared across all callback runs. By default: `%{execute_args: execute_args}`.
+  ## Extensibility
+
+  Other modules can have protocol implementations for `Sage`, eg:
+
+    defimpl Sage, for: MyHTTPClient do
+
+
+    end
+
+
+    Example extensions:
+
+    |> run_request(:post, fn )
+
+  ## Benefits
+
+  ### vs `with`
+
+  ## Handling errors
+
+  ### For transaction callback
+
+  ### For compensating function
+
+  What we could do:
+
+  1. Give up. It's essentially to let it fail and let application developer to find this error in logs and fix manually.
+
+  2. Retry. We can reduce amount of manual work by keeping retrying to compensate transaction.
+  Benefits of this approach is not clear. It application has a bug - retries won't help. If connection went down - we need very large timeouts
+  and sometimes internet connection won't recover at all.
+
+  3. Spin off a compensating process and return error. Implement steps 1 or 2 in this process.
+
+
+
+  What if Internet connection went down and compensations are failing?
+
+  One possible solution 1s to make use of software fault tolerant techniques along
+  the lmes of recovery blocks [Ande8la,Horn74a] A recovery block 1s
+  an alternate or secondary block of code that 1s provided m case a
+  failure 1s detected m the primary block If a failure IS detected the
+  system 1s reset to Its pre-primary state and the secondary block 1s executed
+
+  The secondary block 1s designed to achieve the same end as the primary usmg
+  a different algorithm or technique, hopefully avoiding the primary’s failure
+
+
+  The other possible solution to this problem
+1s manual mterventlon The erroneous transaction
+1s first aborted Then It 1s given to an apphcation
+programmer who, given a descrlptlon of
+the error, can correct it The SEC (or the apphcation)
+then reruns the transactlon and contmues
+processing the saga
+Fortunately, while the transaction 1s bemg
+manually repalred the saga does not hold any
+database resources (1 e , locks) Hence, the fact
+that an already long saga ~111 take even longer
+will not slgmficantly affect performance of other
+transactions
 
   ## Tracing your Sage
 
@@ -11,6 +71,8 @@ defmodule Sage do
   callbacks.
 
   ...
+
+  - `before_and_after_state` is shared across all callback runs. By default: `%{execute_args: execute_args}`.
 
   ## Examples
 
@@ -63,9 +125,10 @@ defmodule Sage do
   @typep effects :: [{name(), any()}]
 
   @typep transaction :: {module(), atom(), [any()]}
-                      | (effects_so_far :: effects() -> {:ok | :error, any()})
+                      | (effects_so_far :: effects(), global_state :: any() -> {:ok | :error, any()})
 
   @typep compensation :: {module(), atom(), [any()]}
+                       | :noop # No side effects to compensate
                        | ({failed_operation :: name(), failed_value :: any(), effects_so_far :: effects()} ->
                                {:ok, any()} # I am compensated my transactions, continue backwards-recovery
                              | {:abort, any()} # I am compensated transaction and want to force backwards recovery on all steps
@@ -75,18 +138,20 @@ defmodule Sage do
                   | ({:ok | :error, effects_so_far :: effects()} -> no_return)
 
   @opaque t :: %__MODULE__{
-    operations: [{name(), transaction(), compensation()}],
-    names: MapSet.t(),
+    operations: [{:run | :run_async, name(), transaction(), compensation()}],
+    operation_names: MapSet.t(),
     direction: :forward | :backward | :force_backward,
     finally: finally(),
-    cursor: name()
+    current_operation: {:transaction, name()}
   }
 
   defstruct [
     operations: [],
     operation_names: MapSet.new(),
-    cursor: nil,
-    direction: :forward
+    direction: :forward,
+    finally: :noop,
+    on_compensation_error: :raise,
+    current_operation: nil
   ]
 
   @typep cache_opts :: [{:adapter, module()}]
@@ -106,18 +171,47 @@ defmodule Sage do
   @spec run(sage :: t(), apply :: transaction(), rollback :: compensation()) :: t()
 
   @doc """
-  Appends sage with an cached transaction and function to compensate it's side effects.
-
-  Cache is stored by calling a `Sage.CacheAdapter` implementation.
-  """
-  @spec run_cached(sage :: t(), apply :: transaction(), rollback :: compensation(), opts :: cache_opts()) :: t()
-
-  @doc """
   Appends sage with an asynchronous transaction and function to compensate it's side effects.
 
   Next non-asynchronous operation will await for this function return.
   """
   @spec run_async(sage :: t(), apply :: transaction(), rollback :: compensation()) :: t()
+
+  @doc """
+  Appends a sage with a function that will be triggered after sage success or abort.
+  """
+  @spec finally(sage :: t(), apply :: finally()) :: t()
+
+  @doc """
+  Merges another sage or adds a step to call external Module to return a sage to merge at run-time.
+  """
+  @spec merge(sage :: t(), sage :: t() | {module(), function(), [any()]}) :: t()
+
+  @doc """
+  Executes a Sage with global state.
+  """
+  @spec execute(sage :: t(), opts :: Keyword.t())
+
+  @doc """
+  Wraps `execute/2` into anonymous function to be run in a Repo transaction.
+  """
+  @spec to_function(sage :: t(), opts :: Keyword.t())
+
+  @doc """
+  Creates a new sage.
+  """
+  @spec new() :: t()
+  def new,
+    do: %Sage{}
+end
+
+defmodule Sage.Experimental do
+  @doc """
+  Appends sage with an cached transaction and function to compensate it's side effects.
+
+  Cache is stored by calling a `Sage.CacheAdapter` implementation.
+  """
+  @spec run_cached(sage :: t(), apply :: transaction(), rollback :: compensation(), opts :: cache_opts()) :: t()
 
   @doc """
   Appends sage with an asynchronous cached transaction and function to compensate it's side effects.
@@ -134,23 +228,31 @@ defmodule Sage do
   Internally this is the same as using:
 
       |> run(fn _ -> {:ok, :noop}, fn state -> if adapter.retry?(state, retry_opts) do {:retry, state} else {:ok, state} end)
+
+  TODO: Also can be used as point of synchronization, eg.
+    - to persist temporary state in a DB to idempotently retry it;
+    - to ack previous execution stage to MQ and create a new one.
+
+  TODO: Rename to retry?
   """
   @spec checkpoint(sage :: t(), retry_opts :: retry_opts()) :: t()
 
   @doc """
-  Appends a sage with a function that will be triggered after sage success or abort.
+  Register function that will handle all critical errors for a compensating functions.
+
+  Internally we will wrap compensation in a try..catch block and handoff error handling to a callback function.
+
+  This allows to implement complex rescuing strategies, eg:
+    - Notify developer about need of manual resolution;
+    - Retries for compensations;
+    - Spin off a compensation process and return an error in the sage.
+    Process can keep retrying to compensate effects by storing sage in state or a persistent storage.
   """
-  @spec finally(sage :: t(), apply :: finally()) :: t()
+  @spec on_compensation_error(sage :: t(), {module(), function(), [any()]}) :: t()
 
   @doc """
-  Merges another sage or adds a step to call external Module to return a sage to merge at run-time.
+  Register persistent adapter and idempotency key generator to make it possible to re-run same requests
+  idempotently (by either replying with old success response or continuing from the latest failed Sage transaction).
   """
-  @spec merge(sage :: t(), sage :: t() | {module(), function(), [any()]}) :: t()
-
-  @doc """
-  Creates a new sage.
-  """
-  @spec new() :: t()
-  def new,
-    do: %Sage{}
+  @spec with_idempotency(sage :: t(), adapter :: module()) :: t()
 end
