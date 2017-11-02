4b1943bcaba8c99e796e593829736bb69d6e1177
Andrew Dryga <andrew@dryga.com>
2017-11-02 16:38:24 +0200

    Dirty POC implementation

--- lib/sage.ex
+++ lib/sage.ex
@@ -1,5 +1,5 @@
 defmodule Sage do
-  @moduledoc """
+  @moduledoc ~S"""
   Sage is an implementation of [Sagas](http://www.cs.cornell.edu/andru/cs711/2002fa/reading/sagas.pdf) pattern
   in pure Elixir. It is go to way when you dealing with distributed transactions, especially with
   an error recovery/cleanup. Sagas guarantees that either all the transactions in a saga are
@@ -91,78 +91,32 @@ defmodule Sage do
   @typep effects :: [{name(), any()}]
 
   @typep transaction :: {module(), atom(), [any()]}
-                      | (effects_so_far :: effects(), global_state :: any() -> {:ok | :error, any()})
+                      | (effects_so_far :: effects(), global_state :: any() -> {:ok | :error | :abort, any()})
 
   @typep compensation :: {module(), atom(), [any()]}
                        | :noop # No side effects to compensate
                        | ({failed_operation :: name(), failed_value :: any(), effects_so_far :: effects()} ->
-                               {:ok, any()} # I am compensated my transactions, continue backwards-recovery
-                             | {:abort, any()} # I am compensated transaction and want to force backwards recovery on all steps
-                             | {:retry, any()}) # I am compensated by transaction, let's retry with this data from my tx
-                             | {:continue, any()} # I am the Circuit Breaker and I know how live wit this error
+                               :ok # I am compensated my transactions, continue backwards-recovery
+                             | :abort # I am compensated transaction and want to force backwards recovery on all steps
+                             | {:retry, Keyword.t}) # I am compensated my transaction, let's retry with this retry opts
+                             | {:continue, any()} # I am the Circuit Breaker and I know how live wit this error. Can be returned only when operation name == failed operation name
 
-  @typep finally :: {module(), atom(), [any()]}
-                  | ({:ok | :error, effects_so_far :: effects()} -> no_return)
+  @typep finally :: [{module(), atom(), [any()]}]
+                  | [({:ok | :error, effects_so_far :: effects()} -> no_return)]
 
-  @opaque t :: %__MODULE__{
-    operations: [{:run | :run_async, name(), transaction(), compensation()}],
+  @type t :: %__MODULE__{
+    operations: [{name(), {:run | :run_async, transaction(), compensation()}}],
     operation_names: MapSet.t(),
-    direction: :forward | :backward | :force_backward,
-    finally: finally(),
-    current_operation: {:transaction, name()}
+    on_compensation_error: :raise | module(),
+    finally: finally()
   }
 
   defstruct [
     operations: [],
     operation_names: MapSet.new(),
-    direction: :forward,
-    finally: :noop,
-    on_compensation_error: :raise,
-    current_operation: nil
+    finally: [],
+    on_compensation_error: :raise
   ]
-
-  @typep cache_opts :: [{:adapter, module()}]
-
-  @typep retry_opts :: [{:adapter, module()},
-                        {:retry_limit, integer()},
-                        {:retry_timeout, integer()}]
-
-  @doc """
-  Appends sage with an transaction that does not have side effects.
-  """
-  @spec run(sage :: t(), apply :: transaction()) :: t()
-
-  @doc """
-  Appends sage with an transaction and function to compensate it's side effects.
-  """
-  @spec run(sage :: t(), apply :: transaction(), rollback :: compensation()) :: t()
-
-  @doc """
-  Appends sage with an asynchronous transaction and function to compensate it's side effects.
-
-  Next non-asynchronous operation will await for this function return.
-  """
-  @spec run_async(sage :: t(), apply :: transaction(), rollback :: compensation()) :: t()
-
-  @doc """
-  Appends a sage with a function that will be triggered after sage success or abort.
-  """
-  @spec finally(sage :: t(), apply :: finally()) :: t()
-
-  @doc """
-  Merges another sage or adds a step to call external Module to return a sage to merge at run-time.
-  """
-  @spec merge(sage :: t(), sage :: t() | {module(), function(), [any()]}) :: t()
-
-  @doc """
-  Executes a Sage with global state.
-  """
-  @spec execute(sage :: t(), opts :: Keyword.t())
-
-  @doc """
-  Wraps `execute/2` into anonymous function to be run in a Repo transaction.
-  """
-  @spec to_function(sage :: t(), opts :: Keyword.t())
 
   @doc """
   Creates a new sage.
@@ -170,4 +124,166 @@ defmodule Sage do
   @spec new() :: t()
   def new,
     do: %Sage{}
+
+  @doc """
+  Appends sage with an transaction that does not have side effects.
+  """
+  @spec run(sage :: t(), name :: name(), apply :: transaction()) :: t()
+  def run(sage, name, transaction),
+    do: add_operation(sage, :run, name, transaction, :noop)
+
+  @doc """
+  Appends sage with an transaction and function to compensate it's side effects.
+  """
+  @spec run(sage :: t(), name :: name(), apply :: transaction(), rollback :: compensation()) :: t()
+  def run(sage, name, transaction, compensation),
+    do: add_operation(sage, :run, name, transaction, compensation)
+
+  @doc """
+  Appends sage with an asynchronous transaction and function to compensate it's side effects.
+
+  Next non-asynchronous operation will await for this function return.
+
+  If there is an error while one or more asynchronous operations are creating transaction,
+  Sage will await for them to complete and compensate created effects.
+  """
+  @spec run_async(sage :: t(), name :: name(), apply :: transaction(), rollback :: compensation()) :: t()
+  def run_async(sage, name, transaction, compensation),
+    do: add_operation(sage, :run_async, name, transaction, compensation)
+
+  @doc """
+  Appends a sage with a function that will be triggered after sage success or abort.
+  """
+  @spec finally(sage :: t(), callback :: finally()) :: t()
+  def finally(%Sage{} = sage, callback) when is_function(callback, 2),
+    do: %{sage | finally: sage.finally ++ [callback]}
+  def finally(%Sage{} = sage, {module, function, arguments})
+    when is_atom(module) and is_atom(function) and is_list(arguments),
+    do: %{sage | finally: sage.finally ++ [{module, function, arguments}]}
+
+  @doc """
+  Merges another sage or adds a step to call external Module to return a sage to merge at run-time.
+  """
+  @spec merge(sage :: t(), sage :: t() | {module(), function(), [any()]}) :: t()
+  def merge(%Sage{}, %Sage{}), do: raise "not implemented"
+  def merge(%Sage{}, {_m, _f, _a}), do: raise "not implemented"
+
+  @doc """
+  Executes a Sage with global state.
+  """
+  @spec execute(sage :: t(), opts :: any()) :: {:ok, result :: any(), effects :: effects()} | {:error, any()}
+  def execute(%Sage{} = sage, opts) do
+    sage.operations
+    |> Enum.reverse()
+    |> execute_transactions([], opts, {nil, %{}, {0, []}, false, nil})
+  end
+
+  defp execute_transactions([{name, operation} | operations], executed_operations, opts, state) do
+    {_last_effect_or_reason, effects_so_far, retries, _abort?, _return} = state
+
+    case execute_transaction(operation, effects_so_far, opts) do
+      {:ok, effect} ->
+        state = {effect, Map.put(effects_so_far, name, effect), retries, false, nil}
+        execute_transactions(operations, [{name, operation} | executed_operations], opts, state)
+
+      {:abort, reason} ->
+        state = {reason, Map.put(effects_so_far, name, reason), retries, true, {name, reason}}
+        execute_compensations([{name, operation} | executed_operations], operations, opts, state)
+
+      {:error, reason} ->
+        state = {reason, Map.put(effects_so_far, name, reason), retries, false, {name, reason}}
+        execute_compensations([{name, operation} | executed_operations], operations, opts, state)
+    end
+  end
+
+  defp execute_transactions([], _executed_operations, _opts, state) do
+    {last_effect, effects_so_far, _retries, _abort?, _return} = state
+    {:ok, last_effect, effects_so_far}
+  end
+
+  defp execute_transaction({:run, operation, _compensation}, effects_so_far, opts) do
+    apply_transaction_fun(operation, effects_so_far, opts)
+  end
+
+  defp execute_compensations([{name, operation} | operations], compensated_operations, opts, state) do
+    {last_effect_or_reason, effects_so_far, retries, abort?, return} = state
+    {effect_to_compensate, effects_so_far} = Map.pop(effects_so_far, name)
+
+    case execute_compensation(operation, effect_to_compensate, opts, state) do
+      :ok ->
+        state = {last_effect_or_reason, effects_so_far, retries, abort?, return}
+        execute_compensations(operations, [{name, operation} | compensated_operations], opts, state)
+
+      :abort ->
+        state = {last_effect_or_reason, effects_so_far, retries, true, return}
+        execute_compensations(operations, [{name, operation} | compensated_operations], opts, state)
+
+      {:retry, retry_opts} ->
+        {count, _retry_opts} = retries
+        if abort? do
+          state = {last_effect_or_reason, effects_so_far, retries, abort?, return}
+          execute_compensations(operations, [{name, operation} | compensated_operations], opts, state)
+        else
+          if Keyword.fetch!(retry_opts, :retry_limit) > count do
+            state = {last_effect_or_reason, effects_so_far, {count + 1, retry_opts}, false, return}
+            execute_transactions([{name, operation} | compensated_operations], operations, opts, state)
+          else
+            state = {last_effect_or_reason, effects_so_far, retries, abort?, return}
+            execute_compensations(operations, [{name, operation} | compensated_operations], opts, state)
+          end
+        end
+
+      {:continue, effect} ->
+        {return_name, _return_reason} = return
+        if return_name == name do
+          state = {effect, Map.put(effects_so_far, name, effect), retries, false, nil}
+          execute_transactions(compensated_operations, [{name, operation} | operations], opts, state)
+        else
+          raise "Circuit breaking is only allowed for continuing compensated transaction"
+        end
+    end
+  end
+
+  defp execute_compensations([], _compensated_operations, _opts, state) do
+    {_last_effect, _effects_so_far, _retries, _abort?, {_name, reason}} = state
+    {:error, reason}
+  end
+
+  defp execute_compensation({:run, _operation, :noop}, _effect_to_compensate, _opts, _state), do: :ok
+  defp execute_compensation({:run, _operation, compensation}, effect_to_compensate, opts, state) do
+    {_last_effect_or_reason, _effects_so_far, _retries, _abort?, {name, reason}} = state
+    apply_compensation_fun(compensation, effect_to_compensate, {name, reason}, opts)
+  end
+
+  defp apply_transaction_fun({mod, fun, args}, effects_so_far, opts),
+    do: apply(mod, fun, [effects_so_far, opts | args])
+  defp apply_transaction_fun(fun, effects_so_far, opts),
+    do: apply(fun, [effects_so_far, opts])
+
+  defp apply_compensation_fun({mod, fun, args}, effect_to_compensate, {name, reason}, opts),
+    do: apply(mod, fun, [effect_to_compensate, {name, reason}, opts | args])
+  defp apply_compensation_fun(fun, effect_to_compensate, {name, reason}, opts),
+    do: apply(fun, [effect_to_compensate, {name, reason}, opts])
+
+  @doc """
+  Wraps `execute/2` into anonymous function to be run in a Repo transaction.
+  """
+  @spec to_function(sage :: t(), opts :: Keyword.t()) :: function()
+  def to_function(%Sage{} = sage, opts),
+    do: fn -> execute(sage, opts) end
+
+  defp add_operation(%Sage{} = sage, type, name, transaction, compensation)
+      when is_atom(name)
+       and (is_function(transaction, 2) or is_tuple(transaction) and tuple_size(transaction) == 3)
+       and (is_function(compensation, 3) or is_tuple(compensation) and tuple_size(compensation) == 3 or compensation == :noop) do
+    %{operations: operations, operation_names: names} = sage
+    if MapSet.member?(names, name) do
+      raise "#{inspect name} is already a member of the Sage: \n#{inspect(sage)}"
+    else
+      %{sage | operations: [{name, {type, transaction, compensation}} | operations],
+               operation_names: MapSet.put(names, name)}
+    end
+  end
 end
+
+# defprotocol inspect for sage
---
+++ test/sage_test.exs
@@ -0,0 +1,193 @@
+defmodule CountingAgent do
+  def start_link(initial) do
+    Agent.start_link fn -> initial end
+  end
+
+  def inc(agent) do
+    Agent.update(agent, fn counter -> counter + 1 end)
+  end
+
+  def dec(agent) do
+    Agent.update(agent, fn counter -> counter - 1 end)
+  end
+
+  def get(agent) do
+    Agent.get(agent, fn counter -> counter end)
+  end
+end
+
+defmodule SideEffectAgent do
+  def start_link do
+    Agent.start_link fn -> [] end
+  end
+
+  def create_side_effect(agent, effect) do
+    Agent.update(agent, fn effects -> [effect | effects] end)
+  end
+
+  def delete_side_effect(agent, effect) do
+    Agent.update(agent, fn effects -> List.delete(effects, effect) end)
+  end
+
+  def side_effects(agent) do
+    Agent.get(agent, fn list -> Enum.reverse(list) end)
+  end
+end
+
+defmodule SageTest do
+  use ExUnit.Case
+  import Sage
+  doctest Sage
+
+  test "applies transactions" do
+    {:ok, agent} = SideEffectAgent.start_link()
+
+    result =
+      new()
+      |> run(:step1, &tx_ok(agent, :t1, &1, &2), &cmp_ok(agent, &1, &2, &3))
+      |> run(:step2, &tx_ok(agent, :t2, &1, &2), &cmp_ok(agent, &1, &2, &3))
+      |> run(:step3, &tx_ok(agent, :t3, &1, &2), &cmp_ok(agent, &1, &2, &3))
+      |> execute([a: :b])
+
+    assert SideEffectAgent.side_effects(agent) == [:t1, :t2, :t3]
+    assert result == {:ok, :t3, %{step1: :t1, step2: :t2, step3: :t3}}
+  end
+
+  test "compensates errors on last step" do
+    {:ok, agent} = SideEffectAgent.start_link()
+
+    result =
+      new()
+      |> run(:step1, &tx_ok(agent, :t1, &1, &2), &cmp_ok(agent, &1, &2, &3))
+      |> run(:step2, &tx_ok(agent, :t2, &1, &2), &cmp_ok(agent, &1, &2, &3))
+      |> run(:step3, &tx_err(agent, :t3, &1, &2), &cmp_ok(agent, &1, &2, &3))
+      |> execute([a: :b])
+
+    assert SideEffectAgent.side_effects(agent) == []
+    assert result == {:error, :t3}
+  end
+
+  test "compensates errors in the middle" do
+    {:ok, agent} = SideEffectAgent.start_link()
+
+    result =
+      new()
+      |> run(:step1, &tx_ok(agent, :t1, &1, &2), &cmp_ok(agent, &1, &2, &3))
+      |> run(:step2, &tx_err(agent, :t2, &1, &2), &cmp_ok(agent, &1, &2, &3))
+      |> run(:step3, &tx_ok(agent, :t3, &1, &2), &cmp_ok(agent, &1, &2, &3))
+      |> execute([a: :b])
+
+    assert SideEffectAgent.side_effects(agent) == []
+    assert result == {:error, :t2}
+  end
+
+  test "reties transactions" do
+    {:ok, agent} = SideEffectAgent.start_link()
+    {:ok, counter} = CountingAgent.start_link(3)
+
+    result =
+      new()
+      |> run(:step1, &tx_ok(agent, :t1, &1, &2), &cmp_retry(agent, &1, &2, &3))
+      |> run(:step2, &tx_ok(agent, :t2, &1, &2), &cmp_ok(agent, &1, &2, &3))
+      |> run(:step3, &tx_err_n_times(agent, counter, :t3, &1, &2), &cmp_ok(agent, &1, &2, &3))
+      |> execute([a: :b])
+
+    assert SideEffectAgent.side_effects(agent) == [:t1, :t2, :t3]
+    assert result == {:ok, :t3, %{step1: :t1, step2: :t2, step3: :t3}}
+  end
+
+  test "reties count" do
+    {:ok, agent} = SideEffectAgent.start_link()
+
+    result =
+      new()
+      |> run(:step1, &tx_ok(agent, :t1, &1, &2), &cmp_ok(agent, &1, &2, &3))
+      |> run(:step2, &tx_ok(agent, :t2, &1, &2), &cmp_retry(agent, &1, &2, &3, 3))
+      |> run(:step3, &tx_err(agent, :t3, &1, &2), &cmp_ok(agent, &1, &2, &3))
+      |> execute([a: :b])
+
+    assert SideEffectAgent.side_effects(agent) == []
+    assert result == {:error, :t3}
+  end
+
+  test "ignores retries on transactions abort" do
+    {:ok, agent} = SideEffectAgent.start_link()
+
+    result =
+      new()
+      |> run(:step1, &tx_ok(agent, :t1, &1, &2), &cmp_retry(agent, &1, &2, &3))
+      |> run(:step2, &tx_ok(agent, :t2, &1, &2), &cmp_ok(agent, &1, &2, &3))
+      |> run(:step3, &tx_err(agent, :t3, &1, &2), &cmp_abort(agent, &1, &2, &3))
+      |> execute([a: :b])
+
+    assert SideEffectAgent.side_effects(agent) == []
+    assert result == {:error, :t3}
+  end
+
+  test "circuit breaker" do
+    {:ok, agent} = SideEffectAgent.start_link()
+
+    result =
+      new()
+      |> run(:step1, &tx_ok(agent, :t1, &1, &2), &cmp_ok(agent, &1, &2, &3))
+      |> run(:step2, &tx_err(agent, :t2, &1, &2), &cmp_continue(agent, &1, &2, &3))
+      |> run(:step3, &tx_ok(agent, :t3, &1, &2), &cmp_ok(agent, &1, &2, &3))
+      |> execute([a: :b])
+
+    assert SideEffectAgent.side_effects(agent) == [:t1, :t3]
+    assert result == {:ok, :t3, %{step1: :t1, step2: :fallback_return, step3: :t3}}
+  end
+
+  def tx_ok(agent_pid, tid, effects_so_far, opts) do
+    SideEffectAgent.create_side_effect(agent_pid, tid)
+    IO.inspect {tid, effects_so_far, opts}, label: "TX: "
+    {:ok, tid}
+  end
+
+  def tx_abort(_agent_pid, tid, effects_so_far, opts) do
+    IO.inspect {tid, effects_so_far, opts}, label: "TX abort: "
+    {:abort, tid}
+  end
+
+  def tx_err(_agent_pid, tid, effects_so_far, opts) do
+    IO.inspect {tid, effects_so_far, opts}, label: "TX error: "
+    {:error, tid}
+  end
+
+  def tx_err_n_times(agent_pid, counter_pid, tid, effects_so_far, opts) do
+    if CountingAgent.get(counter_pid) > 0 do
+      IO.inspect {tid, effects_so_far, opts}, label: "TX error: "
+      CountingAgent.dec(counter_pid)
+      {:error, tid}
+    else
+      tx_ok(agent_pid, tid, effects_so_far, opts)
+    end
+  end
+
+  def cmp_ok(agent_pid, effect_to_compensate, {name, reason}, opts) do
+    SideEffectAgent.delete_side_effect(agent_pid, effect_to_compensate)
+    IO.inspect {effect_to_compensate, {name, reason}, opts}, label: "CMP ok: "
+    :ok
+  end
+
+  # I am compensated by transaction, let's retry with this data from my tx
+  def cmp_retry(agent_pid, effect_to_compensate, {name, reason}, opts, limit \\ 3) do
+    SideEffectAgent.delete_side_effect(agent_pid, effect_to_compensate)
+    IO.inspect {effect_to_compensate, {name, reason}, opts}, label: "CMP retry: "
+    {:retry, [retry_limit: limit]}
+  end
+
+  # I am compensated transaction and want to force backwards recovery on all steps
+  def cmp_abort(agent_pid, effect_to_compensate, {name, reason}, opts) do
+    SideEffectAgent.delete_side_effect(agent_pid, effect_to_compensate)
+    IO.inspect {effect_to_compensate, {name, reason}, opts}, label: "CMP abort: "
+    :abort
+  end
+
+  # I am the Circuit Breaker and I know how live wit this error
+  def cmp_continue(agent_pid, effect_to_compensate, {name, reason}, opts) do
+    SideEffectAgent.delete_side_effect(agent_pid, effect_to_compensate)
+    IO.inspect {effect_to_compensate, {name, reason}, opts}, label: "CMP cont: "
+    {:continue, :fallback_return}
+  end
+end
