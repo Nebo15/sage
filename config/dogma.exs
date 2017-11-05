c0be42ca9c70cc8c4c8c7761edbb7ba012f98672
Andrew Dryga <andrew@dryga.com>
2017-11-05 20:44:56 +0200

    Added configuration for dogma, credo and formatter

---
+++ config/dogma.exs
@@ -0,0 +1,13 @@
+use Mix.Config
+alias Dogma.Rule
+
+config :dogma,
+  rule_set: Dogma.RuleSet.All,
+  exclude: [
+    ~r(\Adeps/)
+  ],
+  override: [
+    %Rule.LineLength{max_length: 120},
+    %Rule.InfixOperatorPadding{enabled: false},
+    %Rule.FunctionArity{max: 5}
+  ]
